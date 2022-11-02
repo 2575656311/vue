ce75796bba5b087e5b082b73b606c81c26583d5e
chaoren110 <2575656311@qq.com>
2022-11-02 11:05:26 +0800

    123

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>{{ shuju }}</h2>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      shuju: "123",
+    };
+  },
+};
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
